c251d8d1c8ff89293507b8c0bace94f0ff78497a
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-09-21 20:11:10 +0400

    Update main.js

--- main.js
+++ main.js
@@ -72,3 +72,65 @@
 // დავწეროთ ფუნქცია რომელსაც გადეცემა რაიმე პარამეტრი და არგუმენტად მიიღებს ისეთ რიცხვებს
 // რომლის დახმარებითაც უნდა გამოვითვალო მაგალითად რამდენი წელი გვაკლდება თითოეულს
 // პენსიამდე და ეს ყველაფერი უნდა დავწეროთ ამ ფუნქციაში.
+
+
+
+
+// function myData(myAge,retirementAge){
+// const years =  retirementAge - myAge;
+// return years;
+// }
+// console.log(myData(21,65))
+
+
+
+// დავამუშავოთ ეს მასივი შესაბამისად ანუ უნდა შეგვეძლოს რომ მივწვდეთ ამ მასივში მაგალითად name
+// და შემდეგ if დავწეროთ რომ თუ მაგალითად ამ მასივში ნამდვილად გვაქვს რომელიმე სახელი დაგვიბეჭდოს
+// რომ ნაპოვნია და მისი ასაკიც არის შესაბამისი რაც მასივში გვიწერია, და ასევე მისი თვალის ფერი 
+// არის და შესაბამისი eye_color,ამას უკვე template literals მეთოდით გავაკეთებდი.აქ უბრალოდ 
+// array ერთერთი მეთოდი დაგეხმარებოდათ და ეცადეთ მასივების დასამუშავბლად რა მეთოდი გამოიყენება 
+// ყველაზე ხშირად თქვენით მოძებნოთ მარტივია ძაან.
+
+// const characters = [
+//     {
+//       name: "Luke Skywalker",
+//       height: "172",
+//       mass: "77",
+//       age:45,
+//       eye_color: "blue",
+//       gender: "male",
+//     },
+//     {
+//       name: "Darth Vader",
+//       height: "202",
+//       mass: "136",
+//       age:20,
+//       eye_color: "yellow",
+//       gender: "male",
+//     },
+//     {
+//       name: "Leia Organa",
+//       height: "150",
+//       age:25,
+//       mass: "49",
+//       eye_color: "brown",
+//       gender: "female",
+//     },
+//     {
+//       name: "Anakin Skywalker",
+//       height: "188",
+//       mass: "84",
+//       age:33,
+//       eye_color: "blue",
+//       gender: "male",
+//     },
+//   ]
+//   const Name = "Leia Organa";
+  
+//   const character = characters.find((char) => char.name === Name);
+  
+//   if (character) {
+//     console.log(`${character.name} has been found. age: ${character.age}. eye Color: ${character.eye_color}`);
+//   } else {
+//     console.log(`${Name} was not found.`);
+//   }
